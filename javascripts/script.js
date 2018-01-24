36f6f1ec2f6e9d11d249aa0d3015bd7e7fc10817
Alrick <alrick.deillon@unifr.ch>
2018-01-24 11:36:17 +0100

    get social info on homepage

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,14 @@
 $(function () {
+  // bs tooltip
   $('[data-toggle="tooltip"]').tooltip()
+
+  // instafeed config
+  var feed = new Instafeed({
+    get: 'user',
+    userId: '***REMOVED***',
+    accessToken: '***REMOVED***.***REMOVED***.***REMOVED***',
+    sortBy: 'most-recent',
+    limit: 2
+  });
+  feed.run();
 })
